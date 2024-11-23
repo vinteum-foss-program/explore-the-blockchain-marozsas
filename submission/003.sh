9bd40979089863af097d97b961fc76326db86a69
Miguel Rozsas <miguel@rozsas.eng.br>
2024-11-23 16:58:57 -0300

    fixing 003, 2nd attempt

--- submission/003.sh
+++ submission/003.sh
@@ -2,6 +2,13 @@
 block="123456"
 # bitcoin-cli getblockstats "$block" '[ "utxo_increase" ]' | jq '.utxo_increase'
 hash=$(bitcoin-cli getblockhash $block)
-bitcoin-cli getblock $hash  | jq -r '.tx[]' | wc -l
+#txList=$(bitcoin-cli getblock $hash  | jq -r '.tx[]' )
 
+# foreach transaction on the block count how many outputs there is
+count=0
+for tx in $(bitcoin-cli getblock $hash | jq -r '.tx[]'); do
+    n=$(bitcoin-cli getrawtransaction $tx true | jq -r '.vout | length')
+    ((count=count+n))
+done
 
+echo $count
