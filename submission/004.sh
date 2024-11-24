8a82a372f2b35ad5ea8d17570b23752346290eb8
Miguel Rozsas <miguel@rozsas.eng.br>
2024-11-24 08:55:37 -0300

    fixing 004.sh, first attempt

--- submission/004.sh
+++ submission/004.sh
@@ -2,11 +2,7 @@
 #   `xpub6Cx5tvq6nACSLJdra1A6WjqTo1SgeUZRFqsX5ysEtVBMwhCCRa4kfgFqaT2o1kwL3esB1PsYr3CUdfRZYfLHJunNWUABKftK2NjHUtzDms2`
 
 xpubkey='xpub6Cx5tvq6nACSLJdra1A6WjqTo1SgeUZRFqsX5ysEtVBMwhCCRa4kfgFqaT2o1kwL3esB1PsYr3CUdfRZYfLHJunNWUABKftK2NjHUtzDms2'
-descriptor="tr(${xpubkey}/0/*)"
+descriptor="tr(${xpubkey}/100)"
 checksum=$(bitcoin-cli getdescriptorinfo $descriptor | jq -r '.checksum')
-bitcoin-cli deriveaddresses "${descriptor}#${checksum}" "[100,100]" | jq -r '.[]'
+bitcoin-cli deriveaddresses "${descriptor}#${checksum}"  | jq -r '.[]'
 
-# alternate solution
-# descriptor="tr(${xpubkey}/0/100)"
-# checksum=$(bitcoin-cli getdescriptorinfo $descriptor | jq -r '.checksum')
-# bitcoin-cli deriveaddresses "${descriptor}#${checksum}"| jq -r '.[]'
